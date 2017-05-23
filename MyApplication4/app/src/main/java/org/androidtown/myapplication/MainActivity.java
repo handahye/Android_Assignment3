671aca452518060d911c1c1b97553721bd91895d
handahye <handahye2@naver.com>
2017-05-23 16:15:36 +0900

    commit
    dd

--- MyApplication4/app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -1,16 +1,24 @@
 package org.androidtown.myapplication;
 
-import android.app.Activity; import android.os.Bundle; import android.view.View; import android.widget.Button; import android.widget.EditText; import android.widget.Toast; import java.io.BufferedReader; import java.io.InputStream; import java.io.InputStreamReader; import java.io.OutputStreamWriter;
+import android.app.Activity; import android.os.Bundle;
+import android.os.Environment;
+import android.view.View; import android.widget.Button; import android.widget.EditText; import android.widget.Toast; import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream; import java.io.InputStreamReader; import java.io.OutputStreamWriter;
 
 public class MainActivity extends Activity {
     private final static String NOTES = "notes.txt";
     private EditText txtData;
     private Button button, button2, button3, button4;
+    File file;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         txtData = (EditText) findViewById(R.id.txtData);
         button = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
@@ -37,12 +45,17 @@ public class MainActivity extends Activity {
                 finishAPP();
             }
         });
+        File sdCard = Environment.getExternalStorageDirectory();
+        File directory = new File (sdCard.getAbsolutePath() + "/MyFiles");
+        directory.mkdirs();
+        file = new File(directory, "textfile.txt");
 
 
     }
     public void write(){
         try {
-            OutputStreamWriter out = new OutputStreamWriter(openFileOutput(NOTES, 0));//아웃풋 스트림 객체 생성
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter out = new OutputStreamWriter(fOut);//아웃풋 스트림 객체 생성
             out.write(txtData.getText().toString());//txtData를 텍스트로 얻어서 문자열로 변환하고 출력 스트림에 씀
             Toast.makeText(this, "WRITE", Toast.LENGTH_SHORT).show();
             out.close();
@@ -55,16 +68,18 @@ public class MainActivity extends Activity {
     }
     public void read(){
         try {
-            InputStream in = openFileInput(NOTES);//파일이름
-            if (in != null) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            FileInputStream fIn = new FileInputStream(file);
+            //InputStream in = openFileInput(NOTES);//파일이름
+            if (fIn != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(fIn));//버퍼리더로 파일을 넣음
                 String str = "";
                 StringBuffer buf = new StringBuffer();
 
                 while ((str = reader.readLine()) != null) {//라인씩 읽어오는데 만약 데이터가 널이 아니면
                     buf.append(str + "\n");
                 }
-                in.close();
+                fIn.close();
                 txtData.setText(buf.toString());//editText에 읽어온 스트링을 반환함
             }
             } catch (java.io.FileNotFoundException e) {
